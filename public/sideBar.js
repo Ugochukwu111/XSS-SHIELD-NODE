e8dbcbf0f5a02b2a44f291d15e944e9c7465c792
Pascal <pas111cal3@gmail.com>
2026-04-07 08:23:48 +0100

    feat: add sidebar

---
+++ public/sideBar.js
@@ -0,0 +1,50 @@
+
+/*I put the below code outside function scope for cache*/ 
+const sideBarBtn = document.getElementById('side-bar-btn'); //side bar button element
+
+const sideBarEl = document.getElementById('side-bar');//sidebar element
+
+const sideBarWrapper = document.getElementById('side-bar-wrapper');
+
+
+
+function toggleSideBar() {
+
+  let isOpen = sideBarEl.getAttribute('aria-label');
+
+  if(isOpen == 'side bar closed'){
+
+    sideBarEl.classList.add('show')
+    sideBarWrapper.style.display = 'flex';
+    sideBarEl.setAttribute('aria-label', 'side bar open');
+    
+
+  }else if (isOpen == 'side bar open') {
+
+    sideBarEl.classList.remove('show');
+    sideBarWrapper.style.display = 'none';
+    sideBarEl.setAttribute('aria-label', 'side bar closed');
+
+  }
+}
+
+
+
+sideBarBtn.addEventListener('click', ()=>{
+  toggleSideBar()
+})
+
+sideBarWrapper.addEventListener('click', ()=>{
+  toggleSideBar()
+})
+
+// const mediaQuery = window.matchMedia('(max-width: 851px)');
+
+// mediaQuery.addEventListener('change', (e) => {
+//   if (e.matches) {
+//     sideBarEl.style.transform = 'translateX(0%)';
+//   }
+// });
+
+
+
